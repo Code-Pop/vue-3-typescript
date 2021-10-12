2ba96dabd0cbc3bfb73b8f7422944cc7a2d1ed29
Andy <andy@Andys-MacBook-Pro.local>
2021-10-12 14:15:48 -0400

    type inference

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-</template>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
 
-<script lang="ts">
+const count = ref(0)
+
+const appInfo = reactive({
+  name: 'Counter',
+  slogan: 'an app you can count on'
+})
+
 </script>
 
+<template>
+  <div>
+    <h1>{{ appInfo.name }}</h1>
+    <h2>{{ appInfo.slogan }}</h2>
+  </div>
+	<p>{{ count }}</p>
+</template>
