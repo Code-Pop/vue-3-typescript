2b21b41acd39e43af748d7e32038e248ccb3c42f
Andy <andy@Andys-MacBook-Pro.local>
2021-10-12 16:28:36 -0400

    extract Counter.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue'
-import fetchCount from './services/fetchCount'
+import { reactive } from 'vue'
+import Counter from './components/Counter.vue'
 
 interface AppInfo {
   name: string,
   slogan: string
 }
 
-const count = ref<number | null>(null)
-
 const appInfo: AppInfo = reactive({
   name: 'Counter',
   slogan: 'an app you can count on'
 })
 
-onMounted(() => {
-	fetchCount((initialCount) => {
-		count.value = initialCount
-	})
-})
-
-function addCount(num: number) {
-  if (count.value !== null) {
-    count.value += num
-  }
-}
 </script>
 
 <template>
@@ -32,8 +19,5 @@ function addCount(num: number) {
     <h1>{{ appInfo.name }}</h1>
     <h2>{{ appInfo.slogan }}</h2>
   </div>
-	<p>{{ count }}</p>
-  <p>
-    <button @click="addCount(1)">Add</button>
-  </p>
+  <Counter></Counter>
 </template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import fetchCount from '../services/fetchCount'
+
+const count = ref<number | null>(null)
+
+onMounted(() => {
+  fetchCount((initialCount) => {
+    count.value = initialCount
+  })
+})
+
+function addCount(num: number) {
+  if (count.value !== null) {
+    count.value += num
+  }
+}
+
+</script>
+
+<template>
+  <p>{{ count }}</p>
+  <p>
+    <button @click="addCount(1)">Add</button>
+  </p>
+</template>
