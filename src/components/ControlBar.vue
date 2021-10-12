75dcf3a4e83698769b4e0ed782a56af6b3e3ed95
Andy <andy@Andys-MacBook-Pro.local>
2021-10-12 16:36:16 -0400

    emit

---
+++ src/components/ControlBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const emit = defineEmits<{ 
+  (event: 'add-count', num: number): void  
+  (event: 'reset-count'): void 
+}>()
+
+</script>
+
+<template>
+  <p>
+    <button @click="emit('add-count', 1)">Add</button>
+    <button @click="emit('reset-count')">Reset</button>
+  </p>
+</template>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import fetchCount from '../services/fetchCount'
+import ControlBar from './ControlBar.vue'
 
 interface Props {
   limit: number,
@@ -34,7 +35,8 @@ function addCount(num: number) {
 
 <template>
   <p>{{ count }}</p>
-  <p>
-    <button @click="addCount(1)">Add</button>
-  </p>
+  <ControlBar 
+    @add-count="addCount"
+    @reset-count="count = 0"
+  ></ControlBar>
 </template>
